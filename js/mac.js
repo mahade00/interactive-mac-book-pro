fbbfdcb4ac4b39a9769d2d1cedde1a37c15ade00
Mahade Hassan <mahade.hassan.bd@gmail.com>
2022-06-26 15:45:37 +0600

    id add

--- js/mac.js
+++ js/mac.js
@@ -1,15 +1,10 @@
-document.getElementById('eight-gb-memory').addEventListener('click', function () {
+/* document.getElementById('eight-gb-memory').addEventListener('click', function () {
     // console.log('8 gb memory clicked');
 
     // general price calculation
     const generalPrice = document.getElementById('general-price');
     const generalPriceText = generalPrice.innerText;
     const generalPriceAmount = parseFloat(generalPriceText);
-
-    let eightGBMemory = document.getElementById('eight-gb-memory');
-    eightGBMemory = 0;
-
-
 
     const totalPrice = document.getElementById('total-price');
     const totalPriceText = totalPrice.innerText;
@@ -22,4 +17,13 @@ document.getElementById('eight-gb-memory').addEventListener('click', function ()
     const finalTotalPriceCalculation = generalPriceAmount + eightGBMemory;
 
     finalTotalPrice.innerText = finalTotalPriceCalculation;
+}) */
+
+document.getElementById('sixteen-gb-memory').addEventListener('click', function () {
+
+    const extraMemory = document.getElementById('extra-memory');
+    const extraMemoryText = parseFloat(extraMemory.innerText);
+    const extraMemoryPrice = extraMemoryText + 180;
+    extraMemory.innerText = extraMemoryPrice;
 })
+
